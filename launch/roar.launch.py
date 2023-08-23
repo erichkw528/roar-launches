d0cd7c4a47ab4674f860222b91f104640b2ce5b5
Michael Wu <wuxiaohua1011@gmail.com>
2023-08-23 00:36:42 -0700

    lite: base station added

--- launch/roar.launch.py
+++ launch/roar.launch.py
@@ -156,5 +156,16 @@ def generate_launch_description():
                 condition=IfCondition(LaunchConfiguration('record', default=False))
                 )
     ld.add_action(record_node)
+
+
+    """base station client"""
+    should_launch_base_station = DeclareLaunchArgument('base_station', default_value="True", description="Launch base station")
+    ld.add_action(should_launch_base_station)
+    base_station_launch_path: Path = Path(get_package_share_directory("roar-foxglove-interface")) / "launch" / "main.launch.py"
+    assert base_station_launch_path.exists(), f"{base_station_launch_path} does not exist"
+    base_station_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(base_station_launch_path.as_posix()),
+        condition=IfCondition(LaunchConfiguration('base_station', default=False)))
+    ld.add_action(base_station_launch)
     return ld
     
