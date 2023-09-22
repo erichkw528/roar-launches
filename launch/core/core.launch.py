77725aebc5a0f256c70e7c6ec5b08aebc119d3ce
Michael Wu <wuxiaohua1011@gmail.com>
2023-09-21 21:28:14 -0700

    files organized

--- launch/core/core.launch.py
+++ launch/core/core.launch.py
@@ -28,33 +28,33 @@ def generate_launch_description():
         default_value=carla_config_file_path.as_posix(),
         description='Full path to the ROS2 parameters file to use for all launched nodes'))
 
-    # """ URDF """
-    # base_path = Path(get_package_share_directory("roar-urdf"))
-    # urdf_launch_path = base_path / "launch" / "state_publisher.launch.py"
-    # assert urdf_launch_path.exists(), f"{urdf_launch_path} does not exist"
-    # urdf_launch = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(urdf_launch_path.as_posix()),
-    #     launch_arguments={
-    #         "param_file": params_file
-    #     }.items()
-    # )
-    # ld.add_action(urdf_launch)
+    """ URDF """
+    base_path = Path(get_package_share_directory("roar-urdf"))
+    urdf_launch_path = base_path / "launch" / "state_publisher.launch.py"
+    assert urdf_launch_path.exists(), f"{urdf_launch_path} does not exist"
+    urdf_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(urdf_launch_path.as_posix()),
+        launch_arguments={
+            "param_file": params_file
+        }.items()
+    )
+    ld.add_action(urdf_launch)
 
-    # """
-    # Costmap
-    # """
-    # costmap_manager_launch_file_path: Path = (
-    #     Path(get_package_share_directory("costmap_node_manager"))
-    #     / "launch"
-    #     / "costmap_node_manager.launch.py"
-    # )
-    # assert costmap_manager_launch_file_path.exists(), f"{costmap_manager_launch_file_path} does not exist"
-    # costmap_manager = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(costmap_manager_launch_file_path.as_posix()),
-    #     launch_arguments={
-    #         "param_file": params_file
-    #     }.items())
-    # ld.add_action(costmap_manager)
+    """
+    Costmap
+    """
+    costmap_manager_launch_file_path: Path = (
+        Path(get_package_share_directory("costmap_node_manager"))
+        / "launch"
+        / "costmap_node_manager.launch.py"
+    )
+    assert costmap_manager_launch_file_path.exists(), f"{costmap_manager_launch_file_path} does not exist"
+    costmap_manager = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(costmap_manager_launch_file_path.as_posix()),
+        launch_arguments={
+            "param_file": params_file
+        }.items())
+    ld.add_action(costmap_manager)
 
     """ Global Planner """
     global_planner_path = Path(get_package_share_directory("global_planning")) / "launch" / "global_planner_manager.launch.py"
@@ -65,43 +65,43 @@ def generate_launch_description():
         }.items())
     ld.add_action(global_planner_launch)
     
-    # """ Local Planner """
-    # local_planner_manager_launch_file_path: Path = (
-    #     Path(get_package_share_directory("local_planner_manager"))
-    #     / "launch"
-    #     / "local_planner_manager.launch.py"
-    # )
-    # assert local_planner_manager_launch_file_path.exists(), f"{local_planner_manager_launch_file_path} does not exist"
-    # local_planner_manager = IncludeLaunchDescription(PythonLaunchDescriptionSource(
-    #         local_planner_manager_launch_file_path.as_posix()
-    #     ),
-    #     launch_arguments={
-    #         "param_file": params_file
-    #     }.items())
-    # ld.add_action(local_planner_manager)
+    """ Local Planner """
+    local_planner_manager_launch_file_path: Path = (
+        Path(get_package_share_directory("local_planner_manager"))
+        / "launch"
+        / "local_planner_manager.launch.py"
+    )
+    assert local_planner_manager_launch_file_path.exists(), f"{local_planner_manager_launch_file_path} does not exist"
+    local_planner_manager = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+            local_planner_manager_launch_file_path.as_posix()
+        ),
+        launch_arguments={
+            "param_file": params_file
+        }.items())
+    ld.add_action(local_planner_manager)
 
 
-    # """ Safety Controller """
-    # controller_manager_launch_file_path: Path = (Path(get_package_share_directory("controller_manager")) / "launch" / "controller.launch.py")
-    # assert controller_manager_launch_file_path.exists(), f"{controller_manager_launch_file_path} does not exist"
-    # controller_manager = IncludeLaunchDescription(PythonLaunchDescriptionSource(controller_manager_launch_file_path.as_posix()),
-    #     launch_arguments={
-    #         "param_file": params_file,
-    #         "manual_control": manual_control
-    #     }.items())
-    # ld.add_action(controller_manager)
-    # ld.add_action(LogInfo(msg=["Controller Manager launched"]))
+    """ Safety Controller """
+    controller_manager_launch_file_path: Path = (Path(get_package_share_directory("controller_manager")) / "launch" / "controller.launch.py")
+    assert controller_manager_launch_file_path.exists(), f"{controller_manager_launch_file_path} does not exist"
+    controller_manager = IncludeLaunchDescription(PythonLaunchDescriptionSource(controller_manager_launch_file_path.as_posix()),
+        launch_arguments={
+            "param_file": params_file,
+            "manual_control": manual_control
+        }.items())
+    ld.add_action(controller_manager)
+    ld.add_action(LogInfo(msg=["Controller Manager launched"]))
 
 
-    # """ State Manager """
-    # state_manager_launch_file_path: Path = (Path(get_package_share_directory("vehicle_state_manager")) / "launch" / "vehicle_state_manager.launch.py")
-    # assert state_manager_launch_file_path.exists(), f"{state_manager_launch_file_path} does not exist"
-    # state_manager = IncludeLaunchDescription(PythonLaunchDescriptionSource(state_manager_launch_file_path.as_posix()),
-    #     launch_arguments={
-    #         "param_file": params_file
-    #     }.items())
-    # ld.add_action(state_manager)
-    # ld.add_action(LogInfo(msg=["State Manager launched"]))
+    """ State Manager """
+    state_manager_launch_file_path: Path = (Path(get_package_share_directory("vehicle_state_manager")) / "launch" / "vehicle_state_manager.launch.py")
+    assert state_manager_launch_file_path.exists(), f"{state_manager_launch_file_path} does not exist"
+    state_manager = IncludeLaunchDescription(PythonLaunchDescriptionSource(state_manager_launch_file_path.as_posix()),
+        launch_arguments={
+            "param_file": params_file
+        }.items())
+    ld.add_action(state_manager)
+    ld.add_action(LogInfo(msg=["State Manager launched"]))
 
 
 
